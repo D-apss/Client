97c86aa77f052981a7b91828dbdecb22db203954
delvin988 <yustin.delvin@gmail.com>
2024-02-21 18:33:19 +0700

    add: feature register and fix style login

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import Layout from "./page/layout.jsx";
 import LoginPage from "./page/login.jsx";
 import Home from "./page/home.jsx";
+import Register from "./page/register.jsx";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const CheckUser = () => {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
     {
         path: "login",
         element: <LoginPage />
+    },
+    {
+        path: "register",
+        element: <Register />
     },
 ]
 )
